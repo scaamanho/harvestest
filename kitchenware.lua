86db06ec597ff4181254504df41f8337ab601b0d
scaamano <scaamanho@gmail.com>
2018-03-05 20:53:49 +0100

    mixing bowl

--- kitchenware.lua
+++ kitchenware.lua
@@ -15,4 +15,17 @@ minetest.register_craftitem("harvestest:bakeware", {
 	description = "Bakeware",
 	inventory_image = "harvestest_bakeware.png",
 })
---doug
+
+-- mixing bowl
+minetest.register_craft({
+  output = "harvestest:mixingbowl",
+  recipe = {{"group:wood","default:stick ","group:wood"},
+  {"","group:wood",""},
+  {"","",""}}
+})
+
+
+minetest.register_craftitem("harvestest:mixingbowl", {
+	description = "Mixing Bowl",
+	inventory_image = "harvestest_mixingbowl.png",
+})
