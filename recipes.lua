f6158aeccf1d45cf21b40f846a8158b80bdd2c45
scaamano <scaamanho@gmail.com>
2018-03-05 22:32:04 +0100

    apple pie

--- recipes.lua
+++ recipes.lua
@@ -10,6 +10,40 @@ minetest.register_craft({
 
 
 minetest.register_craftitem("harvestest:dough", {
-	description = "Dough",
+  description = "Dough",
+  on_use = minetest.item_eat(1),
 	inventory_image = "harvestest_dough.png",
+})
+
+-- apple pie dough
+minetest.register_craft({
+  type = "shapeless",
+  output = "harvestest:applepie_dough",
+  recipe = {"harvestest:bakeware","default:apple","harvestest:sugar","harvestest:dough"},
+})
+
+minetest.register_craftitem("harvestest:applepie_dough", {
+  description = "Apple Pie Dough",
+  on_use = function(itemstack, user, pointed_thing)
+    minetest.do_item_eat(1, "harvestest:bakeware", itemstack, user, pointed_thing)
+  end,
+	inventory_image = "harvestest_applepie_dough.png",
+})
+
+-- apple pie 
+minetest.register_craft({
+	type = "cooking",
+	cooktime = 14,
+	output = "harvestest:applepie",
+  recipe = "harvestest:applepie_dough",
+  --replacements = {{"harvestest:applepie_dough", "harvestest:bakeware"}},
+})
+
+minetest.register_craftitem("harvestest:applepie", {
+  description = "Apple Pie",
+  on_use = function(itemstack, user, pointed_thing)
+    minetest.do_item_eat(10, "harvestest:bakeware", itemstack, user, pointed_thing)
+  end,
+  inventory_image = "harvestest_applepie.png",
+  replacements = {{"harvestest:applepie", "harvestest:bakeware"}},
 })
